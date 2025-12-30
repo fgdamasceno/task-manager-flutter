5f7caf2aaddf6d9ed67a1f448b6d13fc67d95f26
fgdamasceno <fgdamasceno@gmail.com>
2025-12-30 11:08:55 -0300

    chore: add shared_preferences and json serialization to model

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -4,4 +4,24 @@ class Task {
   bool isDone;
 
   Task({required this.id, required this.title, this.isDone = false});
+
+  // Converte um objeto Task para um Mapa (JSON)
+  // Para trabalhar com 'shared_preferences'
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'isDone': isDone,
+    };
+  }
+
+  // Cria um objeto Task a partir de um Mapa (JSON)
+  // Para trabalhar com 'shared_preferences'
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'],
+      title: json['title'],
+      isDone: json['isDone'],
+    );
+  }
 }
